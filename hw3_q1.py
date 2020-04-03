dfc82b937791dd6cc26df344a1864e47fccf62ab
rotemorad <rotem2684@gmail.com>
2020-04-03 22:52:12 +0300

    Arranged code- still need to try the filter method

--- hw3_q1.py
+++ hw3_q1.py
@@ -26,30 +26,34 @@ class FolderIterator:
 
     def _get_readable_files(self):
         """Function checks if the folder is a directory and returns only the files that are readable"""
-        if self.foldername.is_dir():  # folder is a directory- get all sub-directories
-            sub_files = (self.foldername.rglob('*'))
-            readable_files = [file_path for file_path in sub_files if file_path.suffix != '']
-        else:  # folder is not a directory
-            readable_files = [file_path for file_path in self.foldername if file_path.suffix != '']
-        return readable_files
+        sub_files = self.foldername.rglob('*') if self.foldername.is_dir() else self.foldername
+        return [file_path for file_path in sub_files if file_path.suffix != '']
 
     def _get_content(self):
         """Function reads files content and stores them in a dict{file_name: content}"""
         file_content = {}
-        for i in range(len(self.readable_files)):
-            file_content.update({self.readable_files[i].name: self.readable_files[i].read_text()})
+        for file in self.readable_files:
+            file_content.update({file.name: file.read_text()})
         return file_content
 
     def iter_folder(self):
         """Main function to find duplicate and unique files in the filesystem."""
-        uniques = []
-        duplicates = {}
-        repeated_values = set([v for k, v in self.file_content.items()])
-        for value in repeated_values:
-            keys_specified_value = [v for k, v in self.file_content.items() if v == value]
-            uniques.append(keys_specified_value[0])
-            keys_specified_value = [k for k, v in self.file_content.items() if v == value]
-            if len(keys_specified_value) != 1:
-                duplicates.update({keys_specified_value[0]: keys_specified_value[1::]})
-            self.duplicates, self.uniques = (duplicates, uniques)
+        values = set(self.file_content.values())
+        for value in values:
+            #   use filter here :)
+            #   use values instead of items
+            unique_values = [v for v in self.file_content.values() if v == value]
+            self.uniques.append(unique_values[0])
+            keys = [k for k, v in self.file_content.items() if v == value]
+            if len(keys) != 1:
+                self.duplicates.update({keys[0]: keys[1:]})
         return self.duplicates, self.uniques
+
+
+def main():
+    file = FolderIterator('base')
+    print(file.iter_folder())
+
+
+if __name__ == '__main__':
+    main()
