97bcbb3df39ae11af3eb750e90bfd210ddafeb68
rotemorad <rotem2684@gmail.com>
2020-04-03 18:16:37 +0300

    COMMITING

--- h3_q2.py
+++ h3_q2.py
@@ -5,6 +5,17 @@ class Time:
     Represents the time of the day.
     Attributes: hour, minute, second
     """
-    def __init__(self):
-        self.hour = i
-        self.uniques = []  # list instance
+    def __init__(self, hour=00, minute=00, second=00):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def is_int(self):
+        integers = []
+        for item in [self.hour, self.minute, self.second]:
+            if item is int:
+                integers.append(item)
+            if len(integers) == 3:
+                [self.hour, self.minute, self.second] = integers
+
+
