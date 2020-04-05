9c15735b6c35573753347efe323a48fb3693d4d1
rotemorad <rotem2684@gmail.com>
2020-04-05 17:11:55 +0300

    Added a validate_time method

--- hw3_q2.py
+++ hw3_q2.py
@@ -9,38 +9,34 @@ class Time:
         self.minute = minute
         self.second = second
 
-    def check_int(self):
-        """Checks if the hour,minute and second are integers"""
-        [self.hour, self.minute, self.second] = [item if type(item) == int else 0 for item in
-                                                 [self.hour, self.minute, self.second]]
-        return [self.hour, self.minute, self.second]
-
-    def check_positive(self):
-        """Checks if the hour,minute and second are positive numbers"""
-        [self.hour, self.minute, self.second] = [num if num >= 0 else 0 for num in self.check_int()]
-        return [self.hour, self.minute, self.second]
-
-    def check_time(self):
-        """Checks if the hour,minute and second are valid in a 24 hour clock"""
-        list_time = [self.hour, self.minute, self.second]
-        if list_time[0] not in range(24):
-            list_time[0] = 0
-        if list_time[1] not in range(60):
-            list_time[1] = 0
-        if list_time[2] not in range(60):
-            list_time[2] = 0
-        return list_time
+    def validate_time(self):
+        """Checks if the time is valid, if not valid- changes to default time """
+        #  Checks if the hour,minute and second are integers
+        time_integers = [item if type(item) == int else 0 for item in
+                         [self.hour, self.minute, self.second]]
+        #  Checks if the hour,minute and second are positive numbers
+        time_positive_nums = [num if num >= 0 else 0 for num in time_integers]
+        #  Checks if the hour,minute and second are valid in a 24 hour clock
+        if time_positive_nums[0] not in range(24):
+            time_positive_nums[0] = 0
+        if time_positive_nums[1] not in range(60):
+            time_positive_nums[1] = 0
+        if time_positive_nums[2] not in range(60):
+            time_positive_nums[2] = 0
+        [self.hour, self.minute, self.second] = time_positive_nums
+        return self.hour, self.minute, self.second
 
     def add_zero(self):
         """Checks if the hour,minute and second are single digits- if so add 0 in front of them to get a double digit
         number """
         new_format = []
-        for num in [self.hour, self.minute, self.second]:
+        for num in self.validate_time():
             if num in range(10):
                 str_num = str(num)
                 num = str_num.zfill(2)
             new_format.append(num)
-        return new_format
+        [self.hour, self.minute, self.second] = new_format
+        return [self.hour, self.minute, self.second]
 
     def __str__(self):
         str_to_print = f"""The time is:
@@ -49,8 +45,8 @@ class Time:
 
     def is_after(self, other):
         """Checks if time is after other time and returns bool"""
-        time = self.check_time()
-        other_time = other.check_time()
+        time = self.validate_time()
+        other_time = other.validate_time()
         if time != other_time:
             timing = [(time[0] - other_time[0]), (time[1] - other_time[1]), (time[2] - other_time[2])]
             if timing[0] in range(24) and timing[1] in range(60) and timing[2] in range(60):
@@ -58,8 +54,8 @@ class Time:
         return False
 
     def __add__(self, other):
-        time = self.check_time()
-        other_time = other.check_time()
+        time = self.validate_time()
+        other_time = other.validate_time()
         timing = [(time[0] + other_time[0]), (time[1] + other_time[1]), (time[2] + other_time[2])]
         if timing[2] % 60 not in range(timing[2], 60):
             timing[2] = timing[2] % 60
@@ -69,16 +65,15 @@ class Time:
             timing[0] += 1
         if timing[0] % 24 not in range(timing[0], 24):
             timing[0] = timing[0] % 24
-        return timing
+        [self.hour, self.minute, self.second] = timing
+        return [self.hour, self.minute, self.second]
 
 
 def main():
-    time = Time(hour=25)
-    time2 = Time(hour=5, minute=17, second=0)
-    print(time.check_int())
-    print(time.check_positive())
-    print(time.check_time())
+    time = Time(hour=5, minute=30, second=61)
+    print(time.validate_time())
     print(time.__str__())
+    time2 = Time(hour=5, minute=17, second=0)
     print(time.is_after(time2))
     print(time.__add__(time2))
 
