34b6e6ef6da0bdf5115dc023b5efaeddc265b403
rotemorad <rotem2684@gmail.com>
2020-04-06 22:16:26 +0300

    deleted some functions

--- hw3_q2.py
+++ hw3_q2.py
@@ -13,18 +13,17 @@ class Time:
     def validate_time(self):
         """Checks if the time is valid, if not valid- changes it to default time """
         #  Checks if the hour,minute and second are integers
-        time_integers = [item if type(item) == int else 0 for item in
-                         [self.hour, self.minute, self.second]]
+        self.hour, self.minute, self.second = [item if type(item) == int else 0 for item in
+                                               [self.hour, self.minute, self.second]]
         #  Checks if the hour,minute and second are positive numbers
-        time_positive_nums = [num if num >= 0 else 0 for num in time_integers]
+        self.hour, self.minute, self.second = [num if num >= 0 else 0 for num in [self.hour, self.minute, self.second]]
         #  Checks if the hour,minute and second are valid in a 24 hour clock
-        if time_positive_nums[0] not in range(24):
-            time_positive_nums[0] = 0
-        if time_positive_nums[1] not in range(60):
-            time_positive_nums[1] = 0
-        if time_positive_nums[2] not in range(60):
-            time_positive_nums[2] = 0
-        [self.hour, self.minute, self.second] = time_positive_nums
+        if self.hour not in range(24):
+            self.hour = 0
+        if self.minute not in range(60):
+            self.minute = 0
+        if self.second not in range(60):
+            self.second = 0
         return self.hour, self.minute, self.second
 
     def is_after(self, other):
@@ -50,31 +49,7 @@ class Time:
         timing = Time(hour=timing[0], minute=timing[1], second=timing[2])
         return timing
 
-    def add_zero(self):
-        """Checks if the hour,minute and second are single digits- if so add 0 in front of them to get a double digit
-        number """
-        new_format = []
-        for num in self:
-            if num in range(10):
-                str_num = str(num)
-                number = str_num.zfill(2)
-                new_format.append(number)
-            else:
-                new_format.append(num)
-        self.hour, self.minute, self.second = new_format
-        return self.hour, self.minute, self.second
-
     def __str__(self):
         str_to_print = f"""The time is:
-            {self.hour}:{self.minute}:{self.second}"""
+            {self.hour:02d}:{self.minute:02d}:{self.second:02d}"""
         return str_to_print
-
-
-def main():
-    time = Time(0, 0, 0)
-    time2 = Time(0, 0, 1)
-    print(time.is_after(time2))
-
-
-if __name__ == '__main__':
-    main()
