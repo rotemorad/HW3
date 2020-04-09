c0df9f5a15e465b9ee61d21e5105a175919f5361
rotemorad <rotem2684@gmail.com>
2020-04-09 14:12:27 +0300

    2 passed the test, only one function left

--- hw3_q3.py
+++ hw3_q3.py
@@ -3,23 +3,16 @@ import numpy as np
 
 def load_data(fname: str = 'data.npy'):
     """ Load and return an '.npy' file """
-    curr_data = np.load(fname)
-    curr_data.tofile('my_data')
-    return np.load('my_data.npy')
+    return np.load(fname)
 
 
-def find_in_range(data: np.ndarray, num_range: tuple = (0.3, 0.4)):
+def find_in_range(data: np.ndarray = load_data(), num_range: tuple = (0.3, 0.4)):
     """ Return an array containing the values of 'data' that are inside 'num_range' """
-    get_data = load_data()
-    val_in_range = []
-    for num in np.nditer(get_data):
-        if num in num_range:
-            val_in_range.append(num)
-    return np.array(val_in_range)
+    return data[(data < num_range[1]) & (data > num_range[0])]
 
 
 def main():
-    print(load_data())
+    print(find_in_range())
 
 
 if __name__ == '__main__':
