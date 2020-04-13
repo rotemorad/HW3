02cf4e3de20e293e117bb28263119a9747e6efca
rotemorad <rotem2684@gmail.com>
2020-04-13 04:10:09 +0300

    3 passed the test!

--- hw3_q1.py
+++ hw3_q1.py
@@ -21,8 +21,6 @@ class FolderIterator:
         self.foldername = Path(str(foldername))  # pathlib.Path instance
         self.uniques = []  # list instance
         self.duplicates = {}  # dict instance
-        self.readable_files = self._get_readable_files()  # list instance
-        self.file_content = self._get_content()  # dict instance
 
     def _get_readable_files(self):
         """Function checks if the folder is a directory and returns only the files that are readable"""
@@ -31,20 +29,19 @@ class FolderIterator:
 
     def _get_content(self):
         """Function reads files content and stores them in a dict{file_name: content}"""
-        file_content = {}
-        for file in self.readable_files:
-            file_content.update({file.name: file.read_text()})
-        return file_content
+        return {file.name: file.read_text() for file in self._get_readable_files()}
 
     def iter_folder(self):
         """Main function to find duplicate and unique files in the filesystem."""
-        values = set(self.file_content.values())
-        for value in values:
-            #   use filter here :)
-            #   use values instead of items
-            unique_values = [v for v in self.file_content.values() if v == value]
-            self.uniques.append(unique_values[0])
-            keys = [k for k, v in self.file_content.items() if v == value]
-            if len(keys) != 1:
-                self.duplicates.update({keys[0]: keys[1:]})
+        files = self._get_content()
+        for content in set(files.values()):
+            for file in files.items():
+                if file[1] == content:
+                    self.uniques.append(file)
+                    break
+
+            dups_names = [filename for filename, file_content in files.items() if file_content == content]
+            if len(dups_names) > 1:
+                self.duplicates[dups_names[0]] = dups_names[1:]
+
         return self.duplicates, self.uniques
--- hw3_q2.py
+++ hw3_q2.py
@@ -1,3 +1,7 @@
+def valid_time_part(part, max_value):
+    return 0 if type(part) != int or part not in range(max_value) else part
+
+
 class Time:
     """
     Represents the time of the day.
@@ -5,51 +9,36 @@ class Time:
     """
 
     def __init__(self, hour=0, minute=0, second=0):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
-        self.valid_time = self.validate_time()
-
-    def validate_time(self):
-        """Checks if the time is valid, if not valid- changes it to default time """
-        #  Checks if the hour,minute and second are integers
-        self.hour, self.minute, self.second = [item if type(item) == int else 0 for item in
-                                               [self.hour, self.minute, self.second]]
-        #  Checks if the hour,minute and second are positive numbers
-        self.hour, self.minute, self.second = [num if num >= 0 else 0 for num in [self.hour, self.minute, self.second]]
-        #  Checks if the hour,minute and second are valid in a 24 hour clock
-        if self.hour not in range(24):
-            self.hour = 0
-        if self.minute not in range(60):
-            self.minute = 0
-        if self.second not in range(60):
-            self.second = 0
-        return self.hour, self.minute, self.second
+        self.hour = valid_time_part(hour, 24)
+        self.minute = valid_time_part(minute, 60)
+        self.second = valid_time_part(second, 60)
 
     def is_after(self, other):
         """Checks if time is after other time and returns bool"""
-        if self.hour > other.hour:
-            return True
-        elif self.minute > other.minute:
-            return True
-        elif self.second > other.second:
-            return True
-        return False
+        if self.hour != other.hour:
+            return self.hour > other.hour
+
+        if self.minute != other.minute:
+            return self.minute > other.minute
+
+        return self.second > other.second
 
     def __add__(self, other):
-        timing = [(self.hour + other.hour), (self.minute + other.minute), (self.second + other.second)]
-        while timing[2] not in range(60):
-            timing[2] = timing[2] % 60
-            timing[1] += 1
-        while timing[1] not in range(60):
-            timing[1] = timing[1] % 60
-            timing[0] += 1
-        while timing[0] % 24 not in range(24):
-            timing[0] = timing[0] % 24
-        timing = Time(hour=timing[0], minute=timing[1], second=timing[2])
-        return timing
+        total_hours = self.hour + other.hour
+        total_minutes = self.minute + other.minute
+        total_seconds = self.second + other.second
+
+        if total_seconds >= 60:
+            total_seconds %= 60
+            total_minutes += 1
+
+        if total_minutes >= 60:
+            total_minutes %= 60
+            total_hours += 1
+
+        total_hours %= 24
+
+        return Time(hour=total_hours, minute=total_minutes, second=total_seconds)
 
     def __str__(self):
-        str_to_print = f"""The time is:
-            {self.hour:02d}:{self.minute:02d}:{self.second:02d}"""
-        return str_to_print
+        return f"<{self.hour:02d}:{self.minute:02d}:{self.second:02d}>"
